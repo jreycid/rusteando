3ff260e95dc23e956772050f38e1197b43ddd39e
Jose <64285855+jreycid@users.noreply.github.com>
2024-08-18 15:00:36 +0200

    Delete src/_00_modulos.rs

--- src/_00_modulos.rs
+++
@@ -1,7 +0,0 @@
-pub fn hola_mundo() {
-    /*
-        Para coñecer como funciona o sistema de módulos en Rust, ler:
-        https://www.sheshbabu.com/posts/rust-module-system/
-    */
-    println!("\nSaúdos desde un módulo!");
-}
